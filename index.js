aa671a187254801122d7a7e32ab6e19985ace58a
Naveen <pensianaveen70@gmail.com>
2025-08-01 09:58:53 +0000

    added styling to index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
-import router from "./route/router.js"
+import router from "./route/router.js";
+import {fileURLToPath} from "url";
 import path from "path";
 const app= express();
+const __filename= fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename);
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended: true}));
 app.set("views", path.join(path.resolve(), "views"));
+app.use((express.static(path.join(__dirname,"public"))));
 app.use("/",router);
 // app.post('/submit',(req,res)=>{
 //     const name= req.body.userName;
